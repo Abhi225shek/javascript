8112b657ca26c9fe6fb6ff70b2d8559fbed44b9a
Abhi225shek <165900054+Abhi225shek@users.noreply.github.com>
2024-04-13 08:30:23 +0000

    objects

---
+++ 01_basics/object.js
@@ -0,0 +1,9 @@
+let product = {
+    company: "PumA",
+    Size: 'M',
+    price: 1299,
+};
+console.log(product);
+console.log(product.company);
+console.log(product.price);
+console.log(product.Size);
